15ad24af1025598971c62e7503c9042ff1b435fd
l6j6h6lgh <l6j6h6lgh@gmail.com>
2025-05-25 21:19:28 +0900

    N라인 건물 이름 수정

--- lib/utils/building_data.dart
+++ lib/utils/building_data.dart
@@ -5,35 +5,35 @@ import '../models/building.dart';
 import 'package:latlong2/latlong.dart';
 
 final List<Building> buildingList = [
-  Building(name: "수위실", location: LatLng(0.0, 0.0)), // N1
-  Building(name: "법학전문대학원", location: LatLng(0.0, 0.0)), // N2
-  Building(name: "테니스장 관리사", location: LatLng(0.0, 0.0)), // N3
-  Building(name: "산학협력관", location: LatLng(0.0, 0.0)), // N4
-  Building(name: "평생교육원", location: LatLng(0.0, 0.0)), // N5
-  Building(name: "고시원", location: LatLng(0.0, 0.0)), // N6
-  Building(name: "형설관", location: LatLng(0.0, 0.0)), // N7
-  Building(name: "보육교사교육원", location: LatLng(0.0, 0.0)), // N8
-  Building(name: "언어교육관,보육교사교육원", location: LatLng(0.0, 0.0)), // N9
-  Building(name: "대학 본부,국제교류원", location: LatLng(0.0, 0.0)), // N10
-  Building(name: "공동실험실습관", location: LatLng(0.0, 0.0)), // N11
-  Building(name: "중앙도서관", location: LatLng(0.0, 0.0)), // N12
-  Building(name: "경영학관", location: LatLng(0.0, 0.0)), // N13
-  Building(name: "인문사회관(강의동)", location: LatLng(0.0, 0.0)), // N14
-  Building(name: "사회과학대학본관", location: LatLng(0.0, 0.0)), // N15
-  Building(name: "인문대학본관", location: LatLng(0.0, 0.0)), // N16-1
-  Building(name: "미술관", location: LatLng(0.0, 0.0)), // N16-2
-  Building(name: "미술과", location: LatLng(0.0, 0.0)), // N16-3
-  Building(name: "개성재(수위실)", location: LatLng(0.0, 0.0)), // N17-1
-  Building(name: "개성재 관리동", location: LatLng(0.0, 0.0)), // N17-2
-  Building(name: "개성재(진리관)", location: LatLng(0.0, 0.0)), // N17-3
-  Building(name: "개성재(정의관)", location: LatLng(0.0, 0.0)), // N17-4
-  Building(name: "개성재(개척관)", location: LatLng(0.0, 0.0)), // N17-5
-  Building(name: "계영원", location: LatLng(0.0, 0.0)), // N17-6
-  Building(name: "법학관", location: LatLng(0.0, 0.0)), // N18
-  Building(name: "제2본관", location: LatLng(0.0, 0.0)), // N19
-  Building(name: "생활과학관", location: LatLng(0.0, 0.0)), // N20-1
-  Building(name: "생활과학대학부설ㆍ보육교사교육원어린이집", location: LatLng(0.0, 0.0)), // N20-2
-  Building(name: "은하수식당", location: LatLng(0.0, 0.0)), // N21
+  Building(name: "수위실(N1)", location: LatLng(0.0, 0.0)), // N1
+  Building(name: "법학전문대학원(N2)", location: LatLng(0.0, 0.0)), // N2
+  Building(name: "테니스장 관리실(N3)", location: LatLng(0.0, 0.0)), // N3
+  Building(name: "산학협력관(N4)", location: LatLng(0.0, 0.0)), // N4
+  Building(name: "평생교육원(N5)", location: LatLng(0.0, 0.0)), // N5
+  Building(name: "고시원(N6)", location: LatLng(0.0, 0.0)), // N6
+  Building(name: "형설관(N7)", location: LatLng(0.0, 0.0)), // N7
+  Building(name: "보육교사교육원(N8)", location: LatLng(0.0, 0.0)), // N8
+  Building(name: "언어교육관,보육교사교육원(N9)", location: LatLng(0.0, 0.0)), // N9
+  Building(name: "대학 본부,국제교류원(N10)", location: LatLng(0.0, 0.0)), // N10
+  Building(name: "공동실험실습관(N11)", location: LatLng(0.0, 0.0)), // N11
+  Building(name: "중앙도서관(N12)", location: LatLng(0.0, 0.0)), // N12
+  Building(name: "경영학관(N13)", location: LatLng(0.0, 0.0)), // N13
+  Building(name: "인문사회관(강의동)(N14)", location: LatLng(0.0, 0.0)), // N14
+  Building(name: "사회과학대학본관(N15)", location: LatLng(0.0, 0.0)), // N15
+  Building(name: "인문대학본관(N16-1)", location: LatLng(0.0, 0.0)), // N16-1
+  Building(name: "미술관(N16-2)", location: LatLng(0.0, 0.0)), // N16-2
+  Building(name: "미술과(N16-3)", location: LatLng(0.0, 0.0)), // N16-3
+  Building(name: "개성재(수위실)(N17-1)", location: LatLng(0.0, 0.0)), // N17-1
+  Building(name: "개성재 관리동(N17-2)", location: LatLng(0.0, 0.0)), // N17-2
+  Building(name: "개성재(진리관)(N17-3)", location: LatLng(0.0, 0.0)), // N17-3
+  Building(name: "개성재(정의관)(N17-4)", location: LatLng(0.0, 0.0)), // N17-4
+  Building(name: "개성재(개척관)(N17-5)", location: LatLng(0.0, 0.0)), // N17-5
+  Building(name: "계영원(N17-6)", location: LatLng(0.0, 0.0)), // N17-6
+  Building(name: "법학관(N18)", location: LatLng(0.0, 0.0)), // N18
+  Building(name: "제2본관(N19)", location: LatLng(0.0, 0.0)), // N19
+  Building(name: "생활과학관(N20-1)", location: LatLng(0.0, 0.0)), // N20-1
+  Building(name: "생활과학대학부설ㆍ보육교사교육원어린이집(N20-2)", location: LatLng(0.0, 0.0)), // N20-2
+  Building(name: "은하수식당(N21)", location: LatLng(0.0, 0.0)), // N21
   Building(name: "사범대학실험동", location: LatLng(0.0, 0.0)), // E1-1
   Building(name: "사범대학강의동", location: LatLng(0.0, 0.0)), // E1-2
   Building(name: "개신문화관", location: LatLng(0.0, 0.0)), // E2
