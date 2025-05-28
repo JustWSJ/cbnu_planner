69e744ff858894e35c84becf84ccaff26ce319e8
l6j6h6lgh <l6j6h6lgh@gmail.com>
2025-05-29 00:08:21 +0900

    route_service.dart -2

--- lib/services/route_service.dart
+++ lib/services/route_service.dart
@@ -5,3 +5,11 @@ import 'package:latlong2/latlong.dart';
 class RouteService {
   static const _apiKey = '5b3ce3597851110001cf6248c8b7b039e91545918016bbf70f29574a'; // 여기에 키 입력
   static const _baseUrl = 'https://api.openrouteservice.org/v2/directions/foot-walking';
+
+  static Future<List<LatLng>> getRoute(LatLng start, LatLng end) async {
+    final response = await http.post(
+      Uri.parse(_baseUrl),
+      headers: {
+        'Authorization': _apiKey,
+        'Content-Type': 'application/json',
+      },
