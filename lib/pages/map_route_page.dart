c5de105c8f48016f474af9b28d944de1f3bc81ff
dtw2191 <dtw2191@gmail.com>
2025-05-28 23:35:00 +0900

    add StatefulWidget

--- lib/pages/map_route_page.dart
+++ lib/pages/map_route_page.dart
@@ -6,3 +6,12 @@ import 'package:latlong2/latlong.dart';
 import '../models/schedule.dart';
 import '../services/map_service.dart';
 import '../utils/building_data.dart';
+
+class MapRoutePage extends StatefulWidget {
+  final List<Schedule> schedules;
+
+  const MapRoutePage({super.key, required this.schedules});
+
+  @override
+  State<MapRoutePage> createState() => _MapRoutePageState();
+}
