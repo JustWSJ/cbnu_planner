b25da1651966ad91537e84d30510976270363f2f
l6j6h6lgh <l6j6h6lgh@gmail.com>
2025-05-30 00:25:59 +0900

    도보 경로로 수정 - 4

--- lib/services/route_service.dart
+++ lib/services/route_service.dart
@@ -57,3 +57,22 @@ class RouteService {
         result |= (b & 0x1f) << shift;
         shift += 5;
       } while (b >= 0x20);
+      int dlat = (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+      lat += dlat;
+
+      shift = 0;
+      result = 0;
+      do {
+        b = encoded.codeUnitAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      int dlng = (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+      lng += dlng;
+
+      points.add(LatLng(lat / 1E5, lng / 1E5));
+    }
+
+    return points;
+  }
+}
