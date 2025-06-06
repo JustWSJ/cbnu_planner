007c1e5263b888065004b368b782c9d2ef4c121e
dtw2191 <dtw2191@gmail.com>
2025-06-06 20:00:02 +0900

    add _ensurePermission

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -1,4 +1,13 @@
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
-class LocationService {}
+class LocationService {
+  static Future<LocationPermission> _ensurePermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      permission = await Geolocator.requestPermission();
+    }
+    return permission;
+  }
+}
