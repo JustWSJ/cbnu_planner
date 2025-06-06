5ff4c31be5d62e1eff7744a2bf8e557b0a567125
dtw2191 <dtw2191@gmail.com>
2025-06-06 19:22:32 +0900

    add saveSchedules

--- lib/services/schedule_storage.dart
+++ lib/services/schedule_storage.dart
@@ -4,4 +4,10 @@ import '../models/schedule.dart';
 
 class ScheduleStorage {
   static const String _key = 'schedules';
+
+  static Future<void> saveSchedules(List<Schedule> schedules) async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> data = schedules.map((s) => jsonEncode(s.toMap())).toList();
+    await prefs.setStringList(_key, data);
+  }
 }
