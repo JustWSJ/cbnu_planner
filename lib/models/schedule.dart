d73242e3b6113d14140dda3a6d5a404c005929b3
dtw2191 <dtw2191@gmail.com>
2025-06-06 19:15:27 +0900

    schedule.dart add tomap function

--- lib/models/schedule.dart
+++ lib/models/schedule.dart
@@ -7,4 +7,13 @@ class Schedule {
   final TimeOfDay time;
 
   Schedule({required this.title, required this.place, required this.time});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'place': place,
+      'hour': time.hour,
+      'minute': time.minute,
+    };
+  }
 }
