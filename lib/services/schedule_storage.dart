c58a6441dd8b4fd053ef6b444472f680263345f8
dtw2191 <dtw2191@gmail.com>
2025-06-06 19:22:15 +0900

    add ScheduleStorage

--- lib/services/schedule_storage.dart
+++ lib/services/schedule_storage.dart
@@ -1,3 +1,7 @@
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/schedule.dart';
+
+class ScheduleStorage {
+  static const String _key = 'schedules';
+}
