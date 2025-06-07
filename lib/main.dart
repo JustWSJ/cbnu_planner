1942f15b8d0949afd18d25f75dcde92b7b649ad6
l6j6h6lgh <l6j6h6lgh@gmail.com>
2025-06-07 17:27:48 +0900

    다크모드 - 1

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,3 @@ void main() {
   runApp(const CampusScheduleApp());
 }
 
-class CampusScheduleApp extends StatelessWidget {
-  const CampusScheduleApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '스마트 캠퍼스 스케줄러',
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-    );
-  }
-}
