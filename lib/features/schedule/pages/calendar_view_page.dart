588af1bf4f8ac6c762d16ecc6c2d12fc7323d746
l6j6h6lgh <l6j6h6lgh@gmail.com>
2025-06-07 19:29:29 +0900

    캘린더 2

--- lib/features/schedule/pages/calendar_view_page.dart
+++ lib/features/schedule/pages/calendar_view_page.dart
@@ -9,3 +9,12 @@ class CalendarViewPage extends StatefulWidget {
   @override
   State<CalendarViewPage> createState() => _CalendarViewPageState();
 }
+
+class _CalendarViewPageState extends State<CalendarViewPage> {
+  late Map<DateTime, List<Schedule>> _events;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  List<Schedule> _selectedEvents = [];
+
+  @override
+  
