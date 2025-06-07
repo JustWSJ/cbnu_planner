34a3df0668f4c8cae5d37921a44b188f74e4b953
l6j6h6lgh <l6j6h6lgh@gmail.com>
2025-06-07 21:02:47 +0900

    test

---
+++ test/features/map/data/building_data_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('building_data dummy', () {
+    expect(1, 1);
+  });
+}
---
+++ test/features/map/services/route_service_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('route_service dummy', () {
+    expect(1, 1);
+  });
+}
